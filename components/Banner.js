cb0bb3b0f7f53a929e59a099ffdfd930864a7dff
Saikat Bhadury <info2programmer@gmail.com>
2021-10-07 15:24:47 +0530

    Design Update

--- components/Banner.js
+++ components/Banner.js
@@ -1,23 +1,51 @@
-import Image from "next/image"
-import { SearchIcon, GlobeAltIcon, MenuIcon, UserIcon, UserCircleIcon } from '@heroicons/react/solid'
+import styled from "styled-components";
+import Image from "next/image";
 
-function Banner() {
-    return (
-        <div className="relative h-[300px] sm:h-[400px] 
-         lg:h-[500px]  xl:h-[540px] 2xl:h-[700px]">
-
-            <Image
-                src="https://links.papareact.com/0fm"
-                layout="fill" objectFit="cover"
-                className="bg-fixed"
-            />
-
-            <div className="absolute top-1/2 w-full text-center">
-                <p className="text-sm sm:text-lg">Not sure where to go? Perfect.</p>
-                <button className="text-purple-500 bg-white px-10 py-4 shadow-md rounded-full font-bold my-3 hover:shadow-xl active:scale-90 duration-150">I'm flexible</button>
-            </div>
-        </div>
-    )
+export default function Banner() {
+  return (
+    <BannerSection>
+      <span>
+        <h2>Not sure where to go? Perfect.</h2>
+        <a href="#" className="btn btn-dark">
+          {"I'm flexible"}
+        </a>
+      </span>
+    </BannerSection>
+  );
 }
 
-export default Banner
+const BannerSection = styled.section`
+  padding: 6rem var(--sidePadding);
+  background: url(/images/banner.jpg);
+  background-size: cover;
+  border-radius: 1rem;
+  color: var(--brown);
+  span {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    max-width: var(--maxWidth);
+  }
+  h2 {
+    font-size: clamp(1.75rem, 3vw, 2.25rem);
+    line-height: 1.2;
+    margin-bottom: 1.5rem;
+    font-weight: 800;
+  }
+  .btn.btn-dark {
+    --bgcolor: var(--brown);
+    --color: var(--yellow);
+  }
+
+  @media (max-width: 36rem) {
+    aspect-ratio: 0.75;
+    background: url(images/banner-sm.jpg);
+    background-size: cover;
+    background-position: center;
+
+    span {
+      align-items: center;
+      text-align: center;
+    }
+  }
+`;
--- pages/index.js
+++ pages/index.js
@@ -1,69 +1,28 @@
-import Head from 'next/head'
-import Banner from '../components/Banner'
-import Footer from '../components/Footer'
-import Header from '../components/Header'
-import LargeCard from '../components/LargeCard'
-import MediumCard from '../components/MediumCard'
-import SmallCard from '../components/SmallCard'
+import Head from "next/head";
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+import Explore from "../components/Explore";
+import Banner from "../components/Banner";
+import Cards from "../components/Cards";
+import Hosting from "../components/Hosting";
+import Footer from "../components/Footer";
+import { live, discover } from "../data";
 
-export default function Home({ exploreData, cardsData }) {
+export default function Home() {
   return (
-    <div className="">
-      <Head>
-        <title>Lets Build Izifiso 2021</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
+    <>
       <Header />
-      <Banner />
 
-      <main className="max-w-7xl mx-auto px-8 sm:px-16">
-        <section className="pt-6">
-          <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
-
-          {/* Explore Nearby Location */}
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-            {exploreData?.map(({ img, location, distance }) => (
-              <SmallCard img={img} location={location} distance={distance} key={img} />
-            ))}
-          </div>
-
-        </section>
-
-        <section className="pt-6">
-          <h2 className="text-4xl font-semibold pb-5">Live anywhere</h2>
-          <div className="flex space-x-3  p-3 -ml-3 overflow-scroll scrollbar-hide overflow-x-auto">
-            {cardsData.map(({ img, title }) => (
-              <MediumCard key={img} title={title} img={img} />
-            ))}
-          </div>
-
-        </section>
-
-        <LargeCard img="http://links.papareact.com/4cj" title="The greatest outdoor" description="where desire gets wind" btnText="Let's Go" />
+      <main>
+        <Hero />
+        <Explore />
+        <Banner />
+        <Cards {...live} />
+        <Cards {...discover} />
+        <Hosting />
       </main>
 
       <Footer />
-    </div>
-  )
-}
-
-
-export async function getStaticProps() {
-  const exploreData = await fetch("https://jsonkeeper.com/b/4G1G")
-    .then(
-      (res) => res.json()
-    )
-
-  const cardsData = await fetch("https://jsonkeeper.com/b/VHHT")
-    .then(
-      (res) => res.json()
-    )
-
-  return {
-    props: {
-      exploreData,
-      cardsData
-    }
-  }
+    </>
+  );
 }
